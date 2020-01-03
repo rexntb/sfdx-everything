1de84c809c4a2de5b7b449f83a2b8ef1b5be336d
Rex_Ntuba <rexntb@gmail.com>
2020-01-03 13:35:31 -0500

    first commit

---
+++ force-app/main/default/aura/helloWorld/helloWorldController.js
@@ -0,0 +1,96 @@
+({
+    init: function (cmp, event, helper) {
+        cmp.set('v.mapMarkers', [
+            {
+                location: {
+                    City: 'Cap-d\'Ail',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom26',
+                title: 'Cap-d\'Ail'
+            },
+            {
+                location: {
+                    City: 'Beaulieu-sur-Mer',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom96',
+                title: 'Beaulieu-sur-Mer'
+            },
+            {
+                location: {
+                    City: 'Saint-Jean-Cap-Ferrat',
+                    Country: 'France'
+                },
+
+                title: 'Saint-Jean-Cap-Ferrat'
+            },
+            {
+                location: {
+                    City: 'Villefranche-sur-Mer',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom92',
+                title: 'Villefranche-sur-Mer'
+            },
+            {
+                location: {
+                    City: 'Antibes',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom61',
+                title: 'Antibes'
+            },
+            {
+                location: {
+                    City: 'Juan-les-Pins',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom74',
+                title: 'Juan-les-Pins'
+            },
+            {
+                location: {
+                    City: 'Cannes',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom3',
+                title: 'Cannes'
+            },
+            {
+                location: {
+                    City: 'Saint-Raphaël',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom54',
+                title: 'Saint-Raphaël'
+            },
+            {
+                location: {
+                    City: 'Fréjus',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom88',
+                title: 'Fréjus'
+            },
+            {
+                location: {
+                    City: 'Sainte-Maxime',
+                    Country: 'France'
+                },
+
+                icon: 'custom:custom92',
+                title: 'Sainte-Maxime'
+            }
+        ]);
+        cmp.set('v.markersTitle', 'Côte d\'Azur');
+    }
+});
